8bc73aafdb8c6acd5654b914bebce56ece6ad789
Muhammad Umair <70869666+muhammadumair862@users.noreply.github.com>
2022-11-05 23:16:04 +0500

    host update

--- AlgoTrading/settings.py
+++ AlgoTrading/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1!0&c#*@#gv30^w#l#zxh-9dod7jw!r-(4l+e-mwqvf(upz@iy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
